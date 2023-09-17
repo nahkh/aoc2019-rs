b9f19b66b3b7238c338e3e0e579b216cf187658e
Juuso Valli <juuso.valli@gmail.com>
2023-09-17 09:59:52 +0200

    Stub for day 3

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+use std::env;
+
 mod input_files;
 mod day01;
 mod day02;
+mod day03;
 
 fn main() {
-    day01::execute();
-    day02::execute();
+    let args: Vec<String> = env::args().collect();
+    let day_arg = &args[1];
+    let target_day = day_arg.parse::<usize>().unwrap();
+    match target_day {
+        1 => day01::execute(),
+        2 => day02::execute(),
+        3 => day03::execute(),
+        _ => panic!("Day {} not implemented", target_day)
+    }
 }
