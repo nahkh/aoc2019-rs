6509291455f9c78d5501e6f867bdee99262164e9
Juuso Valli <juuso.valli@gmail.com>
2023-09-17 21:24:09 +0200

    Day 4, part 2

--- src/day04.rs
+++ src/day04.rs
@@ -1,4 +1,4 @@
-fn number_is_valid(number: i32) -> bool {
+fn number_is_valid_part1(number: i32) -> bool {
     let num1 = number / 100000;
     let num2 = (number % 100000) / 10000;
     let num3 = (number % 10000) / 1000;
@@ -15,10 +15,48 @@ fn number_is_valid(number: i32) -> bool {
     true
 }
 
-fn count_valid_passwords(start: i32, end: i32) -> i32 {
+fn number_is_valid_part2(number: i32) -> bool {
+    let num1 = number / 100000;
+    let num2 = (number % 100000) / 10000;
+    let num3 = (number % 10000) / 1000;
+    let num4 = (number % 1000) / 100;
+    let num5 = (number % 100) / 10;
+    let num6 = number % 10;
+    if num1 > num2 || num2 > num3 || num3 > num4 || num4 > num5 || num5 > num6 {
+        return false;
+    }
+    let numbers = [num1, num2, num3, num4, num5, num6];
+    let mut current_streak = 0;
+    let mut prev_number = -1;
+    for num in numbers {
+        if num == prev_number {
+            current_streak += 1;
+        } else {
+            if current_streak == 2 {
+                return true;
+            }
+            current_streak = 1;
+        }
+        prev_number = num;
+    }
+    
+    current_streak == 2
+}
+
+fn count_valid_passwords_part1(start: i32, end: i32) -> i32 {
     let mut count = 0;
     for num in start..(end+1) {
-        if number_is_valid(num) {
+        if number_is_valid_part1(num) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+fn count_valid_passwords_part2(start: i32, end: i32) -> i32 {
+    let mut count = 0;
+    for num in start..(end+1) {
+        if number_is_valid_part2(num) {
             count += 1;
         }
     }
@@ -26,16 +64,25 @@ fn count_valid_passwords(start: i32, end: i32) -> i32 {
 }
 
 pub fn execute() {
-    let count = count_valid_passwords(108457, 562041);
-    println!("Number of valid passwords in range: {}", count);
+    let count1 = count_valid_passwords_part1(108457, 562041);
+    println!("Part 1: Number of valid passwords in range: {}", count1);
+    let count2 = count_valid_passwords_part2(108457, 562041);
+    println!("Part 1: Number of valid passwords in range: {}", count2);
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn test_number_is_valid() {
-        assert_eq!(crate::day04::number_is_valid(111111), true);
-        assert_eq!(crate::day04::number_is_valid(223450), false);
-        assert_eq!(crate::day04::number_is_valid(123789), false);
+    fn test_number_is_valid_part1() {
+        assert_eq!(crate::day04::number_is_valid_part1(111111), true);
+        assert_eq!(crate::day04::number_is_valid_part1(223450), false);
+        assert_eq!(crate::day04::number_is_valid_part1(123789), false);
+    }
+
+    #[test]
+    fn test_number_is_valid_part2() {
+        assert_eq!(crate::day04::number_is_valid_part2(112233), true);
+        assert_eq!(crate::day04::number_is_valid_part2(123444), false);
+        assert_eq!(crate::day04::number_is_valid_part2(111122), true);
     }
 }
